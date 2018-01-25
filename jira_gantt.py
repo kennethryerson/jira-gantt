73f131f3b29af673c5b70bb5defdf45dce76d820
Kenneth Ryerson <kryerson@vermeer.com>
2018-01-25 13:29:06 -0600

    added JiraConnection class; working JiraVersionChart

--- jira_gantt.py
+++ jira_gantt.py
@@ -1,17 +1,66 @@
 import datetime
 import gantt
+from jira import JIRA
 
 class JiraVersionChart(object):
+    """A chart of Jira project versions"""
     def __init__(self, project):
         self.project = project
 
     def generate_chart(self):
+        """
+        Creates a version chart SVG.
+        """
         p = gantt.Project(name=self.project.name)
         for version in self.project.versions:
-            task = gantt.Task(name=version.name,
-                start=datetime.date(*version.startDate.split('-'),
-                end=datetime.date(*version.releaseDate.split('-'))
-            p.add_task(task)
-        p.make_svg_for_tasks(filename='{0}.svg'.format(self.project.key),
-                             today=datetime.datetime.now())
+            if hasattr(version, 'startDate') and hasattr(version, 'releaseDate'):
+                start_date = (int(x) for x in version.startDate.split('-'))
+                end_date = (int(x) for x in version.releaseDate.split('-'))
+                task = gantt.Task(name=version.name,
+                                  start=datetime.date(*start_date),
+                                  stop=datetime.date(*end_date))
+                p.add_task(task)
+        today = datetime.date.today()
+        p.make_svg_for_tasks(filename='{0}_dy.svg'.format(self.project.key),
+                             today=today,
+                             scale=gantt.DRAW_WITH_DAILY_SCALE)
+        p.make_svg_for_tasks(filename='{0}_wk.svg'.format(self.project.key),
+                             today=today,
+                             scale=gantt.DRAW_WITH_WEEKLY_SCALE)
 
+class JiraConnection(object):
+    """Manages a connection to a Jira database"""
+    def __init__(self):
+        self.jira = None
+        self.project = None
+
+    def connect(self, url, user, passwd):
+        """
+        Connect to a Jira server
+
+        Arguments:
+        url    -- URL of the Jira server
+        user   -- username
+        passwd -- password
+        """
+        options = {'server': url}
+        self.jira = JIRA(options, basic_auth=(user, passwd))
+
+    def get_project_list(self):
+        """
+        Returns a list of available projects.
+        Projects are tuples with the format (key, name).
+        """
+        plist = []
+        for project in self.jira.projects():
+            plist.append((project.key, project.name))
+        return plist
+
+    def set_project(self, key):
+        """
+        Sets the active project.
+
+        Arguments:
+        key -- project key
+        """
+        self.project = self.jira.project(key)
