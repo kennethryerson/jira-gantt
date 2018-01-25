526ea717981ae54112a5df8597a5f917ee6dd97f
Kenneth Ryerson <kryerson@vermeer.com>
2018-01-25 17:04:42 -0600

    added JiraEpicChart

--- jira_gantt.py
+++ jira_gantt.py
@@ -28,6 +28,34 @@ class JiraVersionChart(object):
                              today=today,
                              scale=gantt.DRAW_WITH_WEEKLY_SCALE)
 
+class JiraEpicChart(object):
+    """A chart of Jira project epics"""
+    def __init__(self, project, epics):
+        self.project = project
+        self.epics = epics
+
+    def generate_chart(self):
+        """
+        Creates an epic chart SVG.
+        """
+        p = gantt.Project(name=self.project.name)
+        for epic in self.epics:
+            if hasattr(epic.fields, 'startDate') and hasattr(epic.fields, 'releaseDate'):
+                name = epic.fields.customfield_10007
+                start_date = (int(x) for x in epic.fields.startDate.split('-'))
+                end_date = (int(x) for x in epic.fields.releaseDate.split('-'))
+                task = gantt.Task(name=name,
+                                  start=datetime.date(*start_date),
+                                  stop=datetime.date(*end_date))
+                p.add_task(task)
+        today = datetime.date.today()
+        p.make_svg_for_tasks(filename='{0}_dy.svg'.format(self.project.key),
+                             today=today,
+                             scale=gantt.DRAW_WITH_DAILY_SCALE)
+        p.make_svg_for_tasks(filename='{0}_wk.svg'.format(self.project.key),
+                             today=today,
+                             scale=gantt.DRAW_WITH_WEEKLY_SCALE)
+
 class JiraConnection(object):
     """Manages a connection to a Jira database"""
     def __init__(self):
@@ -64,3 +92,17 @@ class JiraConnection(object):
         key -- project key
         """
         self.project = self.jira.project(key)
+    
+    def get_epic_list(self, project=None):
+        """
+        Returns a list of epics for the project.
+
+        Arguments:
+        project -- project key (optional if project is set)
+        """
+        if project is None and self.project is not None:
+            project = self.project.key
+        else:
+            raise ValueError('A project is required')
+
+        return self.jira.search_issues('project={0} and type=Epic'.format(project))
